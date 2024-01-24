966018c12122846daca56c08b15197c0ffcb52e7
Eduard Brahas <eduardbrhas@duck.com>
2024-01-24 14:48:48 +0100

    [ML attempts] Added dynamic weight instancing and imp in train function
    Made everything more adaptive to the data-set to eliminate need to change the code

--- Perceptron.py
+++ Perceptron.py
@@ -2,6 +2,7 @@
 Teaching a perceptron an AND gate or an OR gate
 '''
 import math
+import random
 
 def stable_sigmoid(x):
 
@@ -17,20 +18,31 @@ def stable_sigmoid(x):
 def ReLU(x):
 	return max(0.0, x)
 
-def perceptron(w,x_train):
-    #return stable_sigmoid(w[0]*x_train[0] + w[1]*x_train[1] + tresh)
-    return ReLU(w[0]*x_train[0] + w[1]*x_train[1] + tresh)
+def weights_init(x_train, random_init = True):
+    global weights
+    if random_init == True:
+        weights = [(random.random()) for x in range(len(x_train[1]))]
+    else:
+        weights = [(0.0) for x in range(len(x_train[1]))]
 
-def training(x_train, epochs, early_stop_flag=True):
+def perceptron(w,x_train):
+    weighted_sum = 0
+    for y in range(len(x_train)):
+        for k in range(len(weights)):
+            weighted_sum += w[k]*x_train[y] + w[k]*x_train[y]
+    #return stable_sigmoid(weighted_sum + tresh)
+    return ReLU(weighted_sum + tresh)
+
+def training(x_train, x_expected, epochs, early_stop_flag=True):
     global tresh
     global loss_f
     global new_loss
     for epoch in range(epochs):
-            for y in range(4):
-                weights[0] += lam * (x_train[y][2] - perceptron(weights, x_train[y]))*x_train[y][0]*x_train[y][1]
-                weights[1] += lam * (x_train[y][2] - perceptron(weights, x_train[y]))*x_train[y][1]*x_train[y][0]
-                tresh += lam * (x_train[y][2] - perceptron(weights, x_train[y]))
-                new_loss += abs(x_train[y][2] - perceptron(weights, x_train[y]))
+            for y in range(len(x_train)):
+                for k in range(len(weights)):
+                    weights[k] += lam * (x_expected[y] - perceptron(weights, x_train[y]))*x_train[y][k]
+                tresh += lam * (x_expected[y] - perceptron(weights, x_train[y]))
+                new_loss += abs(x_expected[y] - perceptron(weights, x_train[y]))
             if early_stop_flag:
                 if abs(loss_f - new_loss) > 0.0000000005:
                     loss_f = new_loss
@@ -38,8 +50,11 @@ def training(x_train, epochs, early_stop_flag=True):
                     return epoch + 1
     return epochs
 
-x_train_AND = [[1,1,1], [1,0,0], [0,1,0], [0,0,0]]
-x_train_OR = [[1,1,1], [1,0,1], [0,1,1], [0,0,0]]
+x_train_AND = [[1,1], [1,0], [0,1], [0,0]]
+x_train_OR = [[1,1], [1,0], [0,1], [0,0]]
+
+x_expected_AND = [1, 0, 0, 0]
+x_expected_OR = [1, 1, 1, 0]
 
 x_test = [[1,1], [1,0], [0,1], [0,0]]
 
@@ -48,14 +63,16 @@ lam = 0.3
 loss_f = 0
 new_loss = 0
 
-weights = [0.0, 0.0]
+weights = []
 
+weights_init(x_train_AND, True)
 print(f"Before training weights: {weights}")
+
 print ("Before training: ")
 for tests in range(4):
     print (f"{x_test[tests]} => {perceptron(weights,x_test[tests])}") 
 
-print(f"Epoch trained: {training(x_train_AND, 5000, True)}")
+print(f"Epoch trained: {training(x_train_AND, x_expected_AND, 5000, True)}")
 
 print(f"After training weights: {weights}")
 print ("After training results: ")
